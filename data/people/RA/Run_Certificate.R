d8e2f37e6511c87928cb5ce2b61b3f5cbb3193f5
Luca Messerschmidt <l.messerschmidt@zeppelin-university.net>
2020-05-29 11:00:58 +0200

    Update and Certificates

--- data/people/RA/Run_Certificate.R
+++ data/people/RA/Run_Certificate.R
@@ -3,5 +3,5 @@ country_out <- lapply(1:nrow(certificate), function(i) {
   rmarkdown::render("CertificateOfAppreciation.Rmd",
                     output_file=paste0("~/Documents/github/CoronaNet/data/people/RA/Cert/",paste(i,"_"),
                                        substr(str_extract(certificate$Name[i],"[A-Za-z]+"),1,20),".pdf"),
-                    params=list(Name=certificate$Name[i],country=certificate$country[i],Since=certificate$Since[i],End=certificate$End[i],Validating=certificate$Validating[i],Cleaning=certificate$Cleaning[i] )) })
+                    params=list(Name=certificate$Name[i],country=certificate$End[i],Since=certificate$"Start Date"[i],End=certificate$"End Date"[i],Validating=certificate$Validating[i],Cleaning=certificate$Cleaning[i],Special=certificate$Special[i] )) })
 
